d9954164c5bf33b3afcecc9160894ca3cfb00f25
Rohitkalia0 <rohitkalia1110@gmail.com>
2026-03-23 14:52:50 +0530

    Add backend

--- backend/src/exceptions.py
+++ backend/src/exceptions.py
@@ -3,7 +3,8 @@ from http import HTTPStatus
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-
+from typing import List
+from pydantic import BaseModel
 
 class DomainException(Exception):
     def __init__(
@@ -15,15 +16,21 @@ class DomainException(Exception):
         self.message = message
         super().__init__(message)
 
+class FieldViolation(BaseModel):
+    field: str
+    message: str
 
-def error_response(status_code: int, message: str) -> JSONResponse:
+def error_response(status_code: int, message: str, field_violations: List[FieldViolation] = None) -> JSONResponse:
     return JSONResponse(
         status_code=status_code,
         content={
             "success": False,
-            "message": message
+            "message": message,
+            "field_violations": [f.model_dump(mode="json") for f in field_violations] if field_violations else []
         }
     )
+
+
 
 
 def register_exception_handlers(app: FastAPI):
@@ -39,7 +46,20 @@ def register_exception_handlers(app: FastAPI):
         request: Request,
         exc: RequestValidationError
     ) -> JSONResponse:
+        
+        field_violations: List[FieldViolation] = []
+        for error in exc.errors():
+            loc = error.get("loc", [])
+            field_path = ".".join([str(value) for value in loc if value != "body"])
+            field_violations.append(
+            FieldViolation(
+                field=field_path,
+                message=error.get("msg", "Something went wrong")
+            )
+        )
+
         return error_response(
             HTTPStatus.UNPROCESSABLE_CONTENT,
-            HTTPStatus.UNPROCESSABLE_CONTENT.phrase
+            HTTPStatus.UNPROCESSABLE_CONTENT.phrase,
+            field_violations=field_violations
         )
